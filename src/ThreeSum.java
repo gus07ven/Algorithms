6e2a58dc95f716d3f76807e6fba0a2fe9c7f7103
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-05-15 13:50:52 -0500

    Add time measurement instance

--- src/ThreeSum.java
+++ src/ThreeSum.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
 
 public class ThreeSum {
+
+  public static int count(int[] array){
+    HashMap<Integer, Integer> values = new HashMap<Integer, Integer>();
+    for (Integer num : array) {
+      values.put(num, num);
+    }
+
+    int count = 0;
+    for (int i = 0; i < array.length; i++){
+      for (int j = i + 1; j < array.length; j++) {
+        int frontTwoVals = array[i] + array[j];
+        int inverse = values.getOrDefault(-frontTwoVals, Integer.MAX_VALUE);
+        if (frontTwoVals + inverse == 0) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    int[] array = {30, -40, -20, -10, 40, 0, 10, 5};
+    Instant start = Instant.now();
+    System.out.println(ThreeSum.count(array));
+    Instant finish = Instant.now();
+    long elapsedTime = Duration.between(start, finish).toMillis();
+    System.out.println("Elapsed time: " + elapsedTime + " ms");
+  }
 }
