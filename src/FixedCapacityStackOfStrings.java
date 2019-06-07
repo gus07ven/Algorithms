0ddeb706302deab63739e1b6ba93a449350789dd
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-07 10:18:58 -0500

    Modify pop to avoid loitering

--- src/FixedCapacityStackOfStrings.java
+++ src/FixedCapacityStackOfStrings.java
@@ -1,3 +1,5 @@
+// Considerations: we allow null items to be inserted.
+
 public class FixedCapacityStackOfStrings {
 
   private String[] s;
@@ -16,6 +18,8 @@ public class FixedCapacityStackOfStrings {
   }
 
   public String pop(){
-    return s[--N];
+    String item = s[--N];
+    s[N] = null;
+    return item;
   }
 }
