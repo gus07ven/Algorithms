a8ebe43e43ae1b850567d68c6d7ea45bbff81b6d
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-20 12:34:08 -0500

    Add flexible multistack array

--- src/Question3p1.java
+++ src/Question3p1.java
@@ -1,6 +1,7 @@
 import sun.invoke.empty.Empty;
 
 import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
 
 public class Question3p1 {
 
@@ -29,7 +30,6 @@ public class Question3p1 {
       if(isEmpty(stackNum)){
         throw new EmptyStackException();
       }
-
       int topIndex = indexOfTop(stackNum);
       int value = values[topIndex];
       values[topIndex] = 0;
@@ -59,17 +59,90 @@ public class Question3p1 {
     }
   }
 
-  public static void main(String[] args) {
-    Question3p1.FixedMultiStack ms = new Question3p1.FixedMultiStack(3);
-    ms.push(1, 1);
-    ms.push(1, 11);
-    ms.push(1, 111);
-    ms.push(2, 2);
-    ms.push(2, 22);
-    ms.push(2, 222);
-    ms.push(0, 3);
-    ms.push(0, 33);
-    ms.push(0, 333);
+  static class FlexibleMultiStack {
 
+    private SLinkedList.Node[] arrayOfStacks = new SLinkedList.Node[3];
+
+    public FlexibleMultiStack(){
+      arrayOfStacks[0] = null;
+      arrayOfStacks[1] = null;
+      arrayOfStacks[2] = null;
+    }
+
+    public void push(int stackNumber, int value){
+      if(stackNumber < 0 || stackNumber > 2) throw new NoSuchElementException("Use stack numbers 0-2");
+
+      if(isEmpty(stackNumber)) {
+        SLinkedList.Node node = new SLinkedList.Node(value);
+        arrayOfStacks[stackNumber] = node;
+      } else {
+        SLinkedList.Node top = arrayOfStacks[stackNumber];
+        SLinkedList.Node newTop = new SLinkedList.Node(value);
+        newTop.next = top;
+        top = newTop;
+        arrayOfStacks[stackNumber] = top;
+      }
+    }
+
+    public int pop(int stackNumber){
+      if(stackNumber < 0 || stackNumber > 2) throw new NoSuchElementException("Use stack numbers 0-2");
+
+      if(isEmpty(stackNumber)) throw new EmptyStackException();
+
+      SLinkedList.Node top = arrayOfStacks[stackNumber];
+      int value = top.data;
+      SLinkedList.Node newTop = top.next;
+      top = newTop;
+      arrayOfStacks[stackNumber] = top;
+      return value;
+    }
+
+    public int peek(int stackNumber){
+      if(stackNumber < 0 || stackNumber > 2) throw new NoSuchElementException("Use stack numbers 0-2");
+
+      if(isEmpty(stackNumber)) throw new EmptyStackException();
+
+      SLinkedList.Node top = arrayOfStacks[stackNumber];
+      return top.data;
+    }
+
+    public boolean isEmpty(int stackNumber){
+      return arrayOfStacks[stackNumber] == null;
+    }
+  }
+
+  public static void main(String[] args) {
+//    Question3p1.FixedMultiStack ms = new Question3p1.FixedMultiStack(3);
+//    ms.push(1, 1);
+//    ms.push(1, 11);
+//    ms.push(1, 111);
+//    ms.push(2, 2);
+//    ms.push(2, 22);
+//    ms.push(2, 222);
+//    ms.push(0, 3);
+//    ms.push(0, 33);
+//    ms.push(0, 333);
+
+    Question3p1.FlexibleMultiStack fs = new Question3p1.FlexibleMultiStack();
+    fs.push(0, 1);
+    fs.push(0, 2);
+    fs.push(0, 3);
+    fs.push(1, 10);
+    fs.push(1, 11);
+    fs.push(1, 12);
+    fs.push(2, 20);
+    fs.push(2, 21);
+    fs.push(2, 22);
+    fs.push(2, 23);
+    fs.push(2, 24);
+    System.out.println(fs.pop(0));
+    System.out.println(fs.pop(0));
+    System.out.println(fs.pop(2));
+    System.out.println(fs.pop(1));
+    System.out.println(fs.peek(0));
+    System.out.println(fs.peek(1));
+    System.out.println(fs.peek(2));
+    System.out.println(fs.pop(0));
+    System.out.println(fs.pop(0));
   }
 }
