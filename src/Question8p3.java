f0a9440f9b1a35d280771586a71d23678d7c1110
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-25 15:12:05 -0500

    Add find magic index recursively and for non distinct values

--- src/Question8p3.java
+++ src/Question8p3.java
@@ -29,11 +29,40 @@ public class Question8p3 {
     else return magicFast(array, mid + 1, end);
   }
 
+  public static int magicFastNonDistinct(int[] array){
+    return magicFastNonDistinct(array, 0, array.length - 1);
+  }
+
+  public static int magicFastNonDistinct(int[] array, int start, int end){
+    if(end < start){
+      return -1;
+    }
+
+    int midIndex = (start + end) / 2;
+    int midValue = array[midIndex];
+    if(midValue == midIndex) return midIndex;
+
+    // Search left
+    int leftIndex = Math.min(midIndex - 1, midValue);
+    int left = magicFastNonDistinct(array, start, leftIndex);
+    if(left >= 0) return left;
+
+    // Search right
+    int rightIndex = Math.max(midIndex + 1, midValue);
+    int right = magicFastNonDistinct(array, rightIndex, end);
+
+    return right;
+  }
+
   public static void main(String[] args) {
+
     int[] array = {-2, -1, 0, 1, 4, 12, 16};
     System.out.println(findMagicIndex(array));
 
-    int[] array2 = {-10, -5, 2, 4, 5, 7, 8, 9, 12, 13};
+    int[] array2 = {-1, 1, 2, 4, 12, 16};
     System.out.println(magicFast(array2));
+
+    int[] array3 = {-10, -5, 2, 4, 5, 7, 8, 9, 12, 13};
+    System.out.println(magicFastNonDistinct(array3));
   }
 }
