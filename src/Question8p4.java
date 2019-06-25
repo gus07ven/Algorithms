5c04b1a9d5d54ace3d1ef7a94d8cb1a3831c9909
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-25 16:09:09 -0500

    Create power set of a set

---
+++ src/Question8p4.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Question8p4 {
+
+  public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index){
+    ArrayList<ArrayList<Integer>> allsubsets;
+    if(set.size() == index){  // Base case - add empty set
+      allsubsets = new ArrayList<ArrayList<Integer>>();
+      allsubsets.add(new ArrayList<Integer>()); // Empty set
+    } else {
+      allsubsets = getSubsets(set,index + 1);
+      int item = set.get(index);
+      ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<ArrayList<Integer>>();
+      for(ArrayList<Integer> subset : allsubsets){
+        ArrayList<Integer> newsubset = new ArrayList<Integer>();
+        newsubset.addAll(subset);
+        newsubset.add(item);
+        moresubsets.add(newsubset);
+      }
+      allsubsets.addAll(moresubsets);
+    }
+    return allsubsets;
+  }
+
+
+  public static void main(String[] args) {
+    ArrayList<Integer> set = new ArrayList<>();
+    set.add(1);
+    set.add(2);
+    System.out.println(getSubsets(set, 0));
+  }
+}
