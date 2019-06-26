e1d07d8a7e81b1f19355c87ca1bcebcbfd505676
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-26 12:19:33 -0500

    Add tower class

---
+++ src/Tower.java
@@ -0,0 +1,35 @@
+public class Tower {
+
+  private Stack<Integer> disks;
+  private int index;
+
+  public Tower(int i){
+    disks = new Stack<Integer>();
+    index = i;
+  }
+
+  public int index(){
+    return index;
+  }
+
+  public void add(int d){
+    if(!disks.isEmpty() && disks.peek() <= d){
+      System.out.println("Error placing disk " + d);
+    } else {
+      disks.push(d);
+    }
+  }
+
+  public void moveTopTo(Tower t){
+    int top = disks.pop();
+    t.add(top);
+  }
+
+  public void moveDisks(int n, Tower destination, Tower buffer){
+    if(n > 0){
+      moveDisks(n - 1, buffer, destination);
+      moveTopTo(destination);
+      buffer.moveDisks(n - 1, destination, this);
+    }
+  }
+}
