3c3f0edfd0f08593119f72637e60fd3c72884428
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-26 10:17:23 -0500

    Add memoization to minProduct

--- src/Question8p5.java
+++ src/Question8p5.java
@@ -11,31 +11,36 @@ public class Question8p5 {
     else return biggest + multiply2(biggest, smallest - 1);
   }
 
-  public int minProduct(int a, int b){
+  public static int minProduct(int a, int b){
     int bigger = a < b ? b : a;
     int smaller = a < b ? a : b;
-    return minProductHelper(smaller, bigger);
+    int memo[] = new int[smaller + 1];
+    return minProductHelper(smaller, bigger, memo);
   }
 
-  public int minProductHelper(int smaller, int bigger){
+  private static int minProductHelper(int smaller, int bigger, int[] memo){
     if(smaller == 0) {
       return 0;
     } else if(smaller == 1){
       return bigger;
+    } else if(memo[smaller] > 0){
+      return memo[smaller];
     }
     // Compute half. If uneven, compute other half. If even, double it.
     int s = smaller >> 1;
-    int side1 = minProductHelper(s, bigger);
+    int side1 = minProductHelper(s, bigger, memo);
     int side2 = side1;
     if(smaller % 2 == 1){
-      side2 = minProductHelper(smaller - s, bigger);
+      side2 = minProductHelper(smaller - s, bigger, memo);
     }
-    return side1 + side2;
+    memo[smaller] = side1 + side2;
+    return memo[smaller];
   }
 
   public static void main(String[] args) {
-    int num1 = 100;
-    int num2 = 100;
-    System.out.println(multiply(num1, num2));
+    int num1 = 8;
+    int num2 = 9;
+//    System.out.println(multiply(num1, num2));
+    System.out.println(minProduct(num1, num2));
   }
 }
