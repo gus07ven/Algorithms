7d210f721e205014930a53f1b3aaecb585b63923
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-27 13:00:14 -0500

    Add MaxPQ starting code

--- src/MaxPQ.java
+++ src/MaxPQ.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Comparator;
+import java.util.Iterator;
 
-public class MaxPQ {
+public class MaxPQ<T> implements Iterable<T>{
+
+  private T[] pq;                     // Store items at indices 1 to n
+  private int n;                      // Number of items on priority queue
+  private Comparator<T> comparator;   // Optional comparator
+
+  public MaxPQ(T[] keys){
+    n = keys.length;
+    pq = (T[]) new Object[keys.length + 1];
+    for(int i = 0; i < n; i++){
+      pq[i+1] = keys[i];
+    }
+    for(int k = n/2; k >= 1; k--){
+      sink(k);
+    }
+    assert isMaxHeap();
+  }
+
+  public Iterator<T> iterator(){
+    return null;
+  }
 }
