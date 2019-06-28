123216aefe3b7e4cd3ba729bb1bcac2f5283819e
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-06-28 10:20:35 -0500

    Modify PNode class

--- src/PNode.java
+++ src/PNode.java
@@ -1,11 +1,13 @@
-public class PNode<T> {
+public class PNode<K, V> {
 
-  private Comparable<T> key;
-  private T val;
+  private Comparable<K> key;
+  private V val;
   private PNode left, right;
+  int size;
 
-  public PNode(Comparable<T> key, T val){
+  public PNode(Comparable<K> key, V val, int size){
     this.key = key;
     this.val = val;
+    this.size = size;
   }
 }
