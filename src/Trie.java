96f6657ea048f59df556e3a743ab61d54583e3e8
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-01 14:54:49 -0500

    Add Trie class

--- src/Trie.java
+++ src/Trie.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Trie {
+
+  private TrieNode root;
+
+  public Trie(ArrayList<String> list){
+    root = new TrieNode();
+    for(String word : list){
+      root.addWord(word);
+    }
+  }
+
+  public Trie(String[] list){
+    root = new TrieNode();
+    for(String word : list){
+      root.addWord(word);
+    }
+  }
+
+  private boolean contains(String prefix, boolean exact){
+    TrieNode lastNode = root;
+    int i = 0;
+    for(i = 0; i < prefix.length(); i++){
+      lastNode = lastNode.getChild(prefix.charAt(i));
+      if(lastNode == null){
+        return false;
+      }
+    }
+    return !exact || lastNode.terminates();
+  }
+
+  public boolean contains(String prefix){
+    return contains(prefix, false);
+  }
+
+  public TrieNode getRoot(){
+    return root;
+  }
+
+  public static void main(String[] args) {
+
+    ArrayList<String> list = new ArrayList<>();
+    list.add("hello");
+    list.add("help");
+    list.add("hi");
+    list.add("helicopter");
+    list.add("heaven");
+
+    Trie t = new Trie(list);
+    System.out.println(t.contains("heaven"));
+    System.out.println(t.contains("heavy"));
+  }
 }
