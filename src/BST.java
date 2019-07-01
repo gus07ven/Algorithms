6cec2101f24797f60905317dda08162133d5d3ef
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-01 14:28:15 -0500

    Add delete method to BST

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class BST<K extends Comparable<K>, V> {
 
   private PNode root;
@@ -6,8 +8,8 @@ public class BST<K extends Comparable<K>, V> {
 
     private K key;
     private V val;
-    private PNode left, right;
     private int size;
+    private PNode left, right;
 
     public PNode(K key, V val, int size) {
       this.key = key;
@@ -33,6 +35,7 @@ public class BST<K extends Comparable<K>, V> {
   }
 
   // Return value corresponding to given key, or null if no such key.
+  // Best O(log N), worse (N)
   public V get(K key) {
     PNode x = root;
     while (x != null) {
@@ -42,5 +45,69 @@ public class BST<K extends Comparable<K>, V> {
       else return x.val;
     }
     return null;
+  }
+
+  public void put(K key, V val){
+    root = put(root, key, val);
+  }
+
+  // Best O(log N). Worse O(n)
+  private PNode put(PNode x, K key, V val){
+    if(x == null) return new PNode(key, val);
+    int cmp = key.compareTo(x.key);
+    if(cmp < 0){
+      x.left = put(x.left, key, val);
+    } else if(cmp > 0){
+      x.right = put(x.right, key, val);
+    } else {
+      x.val = val;
+    }
+    return x;
+  }
+
+  public K min() {
+    if (isEmpty()) throw new NoSuchElementException("calls min() with empty symbol table");
+    return min(root).key;
+  }
+
+  private PNode min(PNode x) {
+    if (x.left == null) return x;
+    else                return min(x.left);
+  }
+
+  public void deleteMin() {
+    if (isEmpty()) throw new NoSuchElementException("Symbol table underflow");
+    root = deleteMin(root);
+  }
+
+  private PNode deleteMin(PNode x) {
+    if (x.left == null) return x.right;
+    x.left = deleteMin(x.left);
+    x.size = size(x.left) + size(x.right) + 1;
+    return x;
+  }
+
+  // Hibbard deletion. If we allow delete, every operation degenerates to square root of n.
+  // Only a Red-Black Binary Search Tree will guarantee logarithmic performance for all operations.
+  public void delete(K key){
+    root = delete(root, key);
+  }
+
+  private PNode delete(PNode x, K key){
+    if(x == null) return null;
+    int cmp = key.compareTo(x.key);
+    if(cmp < 0) x.left = delete(x.left, key);
+    else if(cmp > 0) x.right = delete(x.right, key);
+    else {
+      if(x.right == null) return x.left;
+      if(x.left == null) return x.right;
+
+      PNode t = x;
+      x = min(t.right);
+      x.right = deleteMin(x.left);
+      x.left = t.left;
+    }
+    x.size = size(x.left) + size(x.right) + 1;
+    return x;
   }
 }
