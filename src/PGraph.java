61d9320542719b1b1c0006b375551e38486070f5
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-02 17:23:27 -0500

    Add methods to PGraph

--- src/PGraph.java
+++ src/PGraph.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class PGraph {
   private static final String NEWLINE = System.getProperty("line.separator");
   private final int V;
@@ -11,6 +13,29 @@ public class PGraph {
     adj = (Bag<Integer>[]) new Bag[vertices];
     for(int v = 0; v < vertices; v++){
       adj[v] = new Bag<Integer>();
+    }
+  }
+
+  public Graph(In in) {
+    try {
+      this.V = in.readInt();
+      if (V < 0) throw new IllegalArgumentException("number of vertices in a Graph must be nonnegative");
+      adj = (Bag<Integer>[]) new Bag[V];
+      for (int v = 0; v < V; v++) {
+        adj[v] = new Bag<Integer>();
+      }
+      int E = in.readInt();
+      if (E < 0) throw new IllegalArgumentException("number of edges in a Graph must be nonnegative");
+      for (int i = 0; i < E; i++) {
+        int v = in.readInt();
+        int w = in.readInt();
+        validateVertex(v);
+        validateVertex(w);
+        addEdge(v, w);
+      }
+    }
+    catch (NoSuchElementException e) {
+      throw new IllegalArgumentException("invalid input format in Graph constructor", e);
     }
   }
 
@@ -34,5 +59,23 @@ public class PGraph {
     E++;
     adj[v].add(w);
     adj[w].add(v);
+  }
+
+  public Iterable<Integer> adj(int v){
+    validateVertex(v);
+    return adj[v];
+  }
+
+  public String toString() {
+    StringBuilder s = new StringBuilder();
+    s.append(V + " vertices, " + E + " edges " + NEWLINE);
+    for (int v = 0; v < V; v++) {
+      s.append(v + ": ");
+      for (int w : adj[v]) {
+        s.append(w + " ");
+      }
+      s.append(NEWLINE);
+    }
+    return s.toString();
   }
 }
