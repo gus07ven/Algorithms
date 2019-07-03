2d62f5058cb94474e770daa81177e0e418214389
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-03 17:19:44 -0500

    Add solution to 4.5

---
+++ src/Question4p5.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Question4p5 {
+
+  public static boolean isBST(TreeNode root){
+    ArrayList<Integer> values = new ArrayList<>();
+    getValuesArray(root, values);
+    return isBST(values);
+  }
+
+  public static void getValuesArray(TreeNode root, ArrayList<Integer> values ){
+    if(root.left != null){
+      getValuesArray(root.left, values);
+    }
+    values.add(root.data);
+    if(root.right != null){
+      getValuesArray(root.right, values);
+    }
+  }
+
+  private static boolean isBST(ArrayList<Integer> values){
+    for(int i = 1; i < values.size(); i++){
+      if(values.get(i) < values.get(i - 1)){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+
+    TreeNode root = new TreeNode(17);
+    root.insert(10);
+    root.right = new TreeNode(3);
+    root.insert(5);
+    root.insert(7);
+    root.insert(2);
+    root.printInOrder();
+
+    System.out.println("Is the tree a BST? " + isBST(root));
+
+  }
+}
