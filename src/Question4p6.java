0c087a8fafa3251be66c749f3f8f00018e2829c7
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-05 11:00:27 -0500

    Add 4.6 solution code

--- src/Question4p6.java
+++ src/Question4p6.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class Question4p6 {
+
+  public static TreeNode inOrderSucc(TreeNode n){
+    if(n == null) return null;
+
+    if(n.right != null){
+      return leftMostChild(n.right);
+    } else {
+      TreeNode q = n;
+      TreeNode x = q.parent;
+      while(x != null && x.left != q){
+        q = x;
+        x = x.parent;
+      }
+      return x;
+    }
+  }
+
+  public static TreeNode leftMostChild(TreeNode n){
+    if(n == null){
+      return null;
+    }
+    while(n.left != null){
+      n = n.left;
+    }
+    return n;
+  }
 }
