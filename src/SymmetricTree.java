ca4b40ae9959ff6455f972a41e370e2c179c30fb
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-08 15:20:28 -0500

    Add iterative solution

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -10,11 +10,26 @@ public class SymmetricTree {
   public boolean isMirror(TreeNode t1, TreeNode t2) {
     if (t1 == null && t2 == null) return true;
     if (t1 == null || t2 == null) return false;
-    return (t1.data == t2.data)
-            && isMirror(t1.right, t2.left)
-            && isMirror(t1.left, t2.right);
+    return (t1.data == t2.data) && isMirror(t1.right, t2.left) && isMirror(t1.left, t2.right);
   }
 
+  public boolean isSymmetricIter(TreeNode root) {
+    Queue<TreeNode> q = new LinkedList<>();
+    q.add(root);
+    q.add(root);
+    while (!q.isEmpty()) {
+      TreeNode t1 = q.poll();
+      TreeNode t2 = q.poll();
+      if (t1 == null && t2 == null) continue;
+      if (t1 == null || t2 == null) return false;
+      if (t1.data != t2.data) return false;
+      q.add(t1.left);
+      q.add(t2.right);
+      q.add(t1.right);
+      q.add(t2.left);
+    }
+    return true;
+  }
 
   public static void main(String[] args) {
     TreeNode root = new TreeNode(1);
@@ -29,6 +44,9 @@ public class SymmetricTree {
     SymmetricTree st = new SymmetricTree();
     System.out.println(st.isSymmetric(root));
 
+    // Iterative solution
+    SymmetricTree st2 = new SymmetricTree();
+    System.out.println(st2.isSymmetricIter(root));
   }
 }
 
