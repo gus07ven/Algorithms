ffe0a0e4d41847d1759d1793e5a850239b6c4502
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-09 10:11:51 -0500

    Another solution to 108

--- src/Leet108.java
+++ src/Leet108.java
@@ -43,9 +43,28 @@ public class Leet108 {
     }
   }
 
+  public static TreeNode sortedArrayToBST2(int[] nums){
+    if (nums.length == 0) {
+      return null;
+    }
+    TreeNode head = helper(nums, 0, nums.length - 1);
+    return head;
+  }
+
+  public static TreeNode helper(int[] num, int low, int high) {
+    if (low > high) { // Done
+      return null;
+    }
+    int mid = low + (high - low) / 2;
+    TreeNode node = new TreeNode(num[mid]);
+    node.left = helper(num, low, mid - 1);
+    node.right = helper(num, mid + 1, high);
+    return node;
+  }
+
   public static void main(String[] args) {
     int[] nums = {-2, -2, -2, 8};
-    TreeNode result = sortedArrayToBST(nums);
+    TreeNode result = sortedArrayToBST2(nums);
     result.printInOrder();
   }
 }
