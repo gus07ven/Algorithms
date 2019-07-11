e6e754388f54f7d9b607edfc89e952bb1f1c98ae
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-11 13:30:14 -0500

    Modify tree class

--- src/Tree.java
+++ src/Tree.java
@@ -13,4 +13,12 @@ public class Tree {
     int i = random.nextInt(size());
     return root.getIthNode(i);
   }
+
+  public void insertInOrder(int value){
+    if(root == null) {
+      root = new TreeNode2(value);
+    } else {
+      root.insertInOrder(value);
+    }
+  }
 }
