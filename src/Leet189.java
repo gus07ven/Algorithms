631bc9ede11e42f9a0544c7e0ff92f296e7db1bc
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-10 20:12:36 -0500

    Rotate array in place solution 2

--- src/Leet189.java
+++ src/Leet189.java
@@ -17,6 +17,23 @@ public class Leet189 {
     }
   }
 
+  public void rotate2(int[] nums, int k) {
+    k = k % nums.length;
+    int count = 0;
+    for (int start = 0; count < nums.length; start++) {
+      int current = start;
+      int prev = nums[start];
+      do {
+        int next = (current + k) % nums.length;
+        int temp = nums[next];
+        nums[next] = prev;
+        prev = temp;
+        current = next;
+        count++;
+      } while (start != current);
+    }
+  }
+
   public static void main(String[] args) {
     int[] nums = {1,2,3,4,5,6,7};
     Leet189 leet = new Leet189();
