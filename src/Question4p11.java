c0ce58824aee8f0407666d6f3e42a55e17ddf25a
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-11 13:32:12 -0500

    Add driver class for 4.11

--- src/Question4p11.java
+++ src/Question4p11.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Question4p11 {
+
+  public static void main(String[] args) {
+   Tree tree = new Tree();
+   tree.insertInOrder(17);
+   tree.insertInOrder(10);
+   tree.insertInOrder(23);
+   tree.insertInOrder(5);
+   tree.insertInOrder(29);
+   tree.insertInOrder(2);
+
+   System.out.println(tree.size());
+
+    for(int i = 0; i < tree.size(); i++) {
+      System.out.println(tree.getRandomNode().data());
+    }
+  }
 }
