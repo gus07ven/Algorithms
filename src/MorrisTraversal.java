6ddc7db7bee102ca57a2eb4a4e57212af7a47695
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-12 14:34:53 -0500

    Add inorder traversal method

--- src/MorrisTraversal.java
+++ src/MorrisTraversal.java
@@ -1,15 +1,15 @@
 public class MorrisTraversal {
 
-  public static void inorder(TreeNode root){
+  public static void inorder(TreeNode root) {
     TreeNode current = root;
 
-    while(current != null){
-      if(current.left == null){
+    while (current != null) {
+      if (current.left == null) {
         System.out.println(current.data);
         current = current.right;
       } else {
         TreeNode predecessor = findPredecessor(current, current.left);
-        if(predecessor.right == null){
+        if (predecessor.right == null) {
           predecessor.right = current;
           current = current.left;
         } else {
@@ -21,10 +21,24 @@ public class MorrisTraversal {
     }
   }
 
-  public TreeNode findPredecessor(TreeNode current, TreeNode predecessor){
-    while(predecessor.right != null && predecessor.right != current){
+  public static TreeNode findPredecessor(TreeNode current, TreeNode predecessor) {
+    while (predecessor.right != null && predecessor.right != current) {
       predecessor = predecessor.right;
     }
     return predecessor;
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(5);
+    root.insert(3);
+    root.insert(9);
+    root.insert(2);
+    root.insert(4);
+    root.insert(1);
+    root.insert(7);
+    root.insert(12);
+    //root.printInOrder();
+
+    inorder(root);
   }
 }
