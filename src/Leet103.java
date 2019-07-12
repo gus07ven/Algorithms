9605ca2fda4b06023076a406d44cfdd3dc2c8b85
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-12 13:01:04 -0500

    Add method to get levels list leet 103

--- src/Leet103.java
+++ src/Leet103.java
@@ -6,7 +6,20 @@ public class Leet103 {
   public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
     List<List<Integer>> lists = new ArrayList<>();
     zzLevelOrderHelper(root, lists, 0);
-    return zzListBuilder(lists);
+    return lists;
+  }
+
+  public void zzLevelOrderHelper(TreeNode root, List<List<Integer>> lists, int level){
+    if(root == null) return;
+    if(level >= lists.size()){
+      List<Integer> curLevelList = new ArrayList<>();
+      curLevelList.add(root.data);
+      lists.add(level, curLevelList);
+    } else {
+      lists.get(level).add(root.data);
+    }
+    zzLevelOrderHelper(root.left, lists, level + 1);
+    zzLevelOrderHelper(root.right, lists, level + 1);
   }
 
 }
