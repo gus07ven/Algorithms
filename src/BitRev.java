15d0e2e2b4c4404d57b1c20fc4339899c56e7737
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-16 09:25:33 -0500

    Add shift methods

--- src/BitRev.java
+++ src/BitRev.java
@@ -1,5 +1,23 @@
 public class BitRev {
 
+  public static int repeatedArithmeticShift(){
+    int x = -93242;
+    int count = 40;
+    for(int i = 0; i < count; i++){
+      x >>= 1;  // Arithmetic shift by 1
+    }
+    return x;
+  }
+
+  public static int repeatedLogicalShift(){
+    int x = -93242;
+    int count = 40;
+    for(int i = 0; i < count; i++){
+      x >>>= 1;  // Logical shift by 1
+    }
+    return x;
+  }
+
   public static void main(String[] args) {
 
     int x = 7;
