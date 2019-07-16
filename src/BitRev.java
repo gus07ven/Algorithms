8bc5776cee2a660342577b7466f648c38c6ad345
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-16 11:30:30 -0500

    Add shift methods

--- src/BitRev.java
+++ src/BitRev.java
@@ -18,6 +18,14 @@ public class BitRev {
     return x;
   }
 
+  public static boolean getBit(int num, int i){
+    return (num & (1 << i)) != 0;
+  }
+
+  public static int setBit(int num, int i){
+    return num | (1 << i);
+  }
+
   public static void main(String[] args) {
 
     int x = 7;
@@ -28,18 +36,29 @@ public class BitRev {
     int test3 = x ^ x;
     int test4 = x ^ y;
 
-    System.out.println(test4);
+//    System.out.println(test4);
 
     int and1 = x & 0;
     int and2 = x & 1;
     int and3 = x & x;
 
-    System.out.println(and2);
+//    System.out.println(and2);
 
     int or1 = x | 0;
     int or2 = x | 1;
     int or3 = x | x;
 
-    System.out.println(or2);
+//    System.out.println(or2);
+
+//    System.out.println(repeatedArithmeticShift());
+//    System.out.println(repeatedLogicalShift());
+
+    System.out.println("Get bit examples: ");
+    System.out.println(getBit(6, 2));
+    System.out.println(getBit(6, 0));
+
+    System.out.println("Set bit examples: ");
+    System.out.println(setBit(6, 2));
+    System.out.println(setBit(6, 0));
   }
 }
