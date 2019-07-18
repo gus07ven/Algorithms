7f781019b0ead2b3bab62ab068e09257ed00bcc9
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-18 11:16:06 -0500

    IsPowerOfThree another solution

--- src/Leet326.java
+++ src/Leet326.java
@@ -4,8 +4,23 @@ public class Leet326 {
     return n > 0 && 1162261467 % n == 0;
   }
 
+  public static boolean isPowerOfThree2(int n){
+    if(n < 1) return false;
+
+    while(n % 3 == 0){
+      n = n / 3;
+    }
+    return n == 1;
+  }
+
   public static void main(String[] args) {
+    System.out.println("O(1) method call results:");
     System.out.println(isPowerOfThree(81));
     System.out.println(isPowerOfThree(4));
+
+    System.out.println();
+    System.out.println("O(log3(n)) results:");
+    System.out.println(isPowerOfThree2(81));
+    System.out.println(isPowerOfThree2(4));
   }
 }
