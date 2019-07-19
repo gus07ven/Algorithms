ebbcfd649957ed161ab7364374a5f933ad05db6f
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-19 11:35:38 -0500

    Add two numbers with out using + or -

--- src/Leet371.java
+++ src/Leet371.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Leet371 {
 
   public static int sum(int a, int b){
@@ -9,7 +11,17 @@ public class Leet371 {
     return a;
   }
 
+  public static int streamSum(int a, int b){
+    return Arrays.stream(new int[] {a, b}).sum();
+  }
+
   public static void main(String[] args) {
+
+    System.out.println("Print imperative sum: ");
     System.out.println(sum(2, 3));
+
+    System.out.println();
+    System.out.println("Print functional sum: ");
+    System.out.println(streamSum(2, 7));
   }
 }
