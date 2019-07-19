8fcad0d5ceed1fe77449b13c1ef9020252030ac8
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-19 15:31:22 -0500

    Imperative solution for hamming distance 461

--- src/Leet461.java
+++ src/Leet461.java
@@ -4,8 +4,23 @@ public class Leet461 {
     return Integer.bitCount(a ^ b);
   }
 
+  public static int HammingDistanceImperative(int x, int y){
+    if(x == 0 && y == 0) return 0;
+
+    int result = x ^ y;
+    int count = 0;
+    for(int i = 0; i < Integer.BYTES * 8; i++){
+      if((result & 1) == 1) {
+        count++;
+      }
+      result >>= 1;
+    }
+    return count;
+  }
+
   public static void main(String[] args) {
     System.out.println("Number of one bits present:");
     System.out.println("Built-in method: " + HammingDistance(0,0));
+    System.out.println("Imperative implementation: " + HammingDistanceImperative(-1,0));
   }
 }
