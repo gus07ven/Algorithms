2276da9f0817ad7ebc4d6986bc3fe78c630dd6a9
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-22 15:20:13 -0500

    Clean Leet 78

--- src/Leet78.java
+++ src/Leet78.java
@@ -1,27 +1,29 @@
-import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Leet78 {
 
   public static List<List<Integer>> subsets(int[] nums) {
     List<List<Integer>> result = new ArrayList<>();
+
     result.add(new ArrayList<>());
 
     for (int num : nums){
+
       int length = result.size();
-      for (int i = 0 ; i < length ; i++){
+      for (int i = 0 ; i < length; i++){
         List<Integer> tmp = new ArrayList<>(result.get(i));
         tmp.add(num);
         result.add(tmp);
       }
     }
+
     return result;
   }
 
   public static void main(String[] args) {
-    int[] nums = {};
+    int[] nums = {1,2,3};
     subsets(nums);
   }
+
 }
