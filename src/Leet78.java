f783497d3db49ffb2496b3d3979d52cb3d9cffbf
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-22 12:25:47 -0500

    Add solution to Leet 78

---
+++ src/Leet78.java
@@ -0,0 +1,27 @@
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leet78 {
+
+  public static List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    result.add(new ArrayList<>());
+
+    for (int num : nums){
+      int length = result.size();
+      for (int i = 0 ; i < length ; i++){
+        List<Integer> tmp = new ArrayList<>(result.get(i));
+        tmp.add(num);
+        result.add(tmp);
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {};
+    subsets(nums);
+  }
+}
