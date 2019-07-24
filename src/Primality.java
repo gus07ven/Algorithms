b1904866e469b5e154179f43a53be4aaee7cf721
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-24 17:33:37 -0500

    Add a better method to find primes

--- src/Primality.java
+++ src/Primality.java
@@ -1,6 +1,6 @@
 public class Primality {
 
-  public static boolean primeNaive(int n){
+  private static boolean primeNaive(int n){
     if(n < 2) return false;
 
     for(int i = 2; i < n; i++){
@@ -11,14 +11,31 @@ public class Primality {
     return true;
   }
 
+  private static boolean primeBetter(int n){
+    if(n < 2) return false;
+
+    int sqrt = (int) Math.sqrt(n);
+    for(int i = 2; i <= sqrt; i++){
+      if(n % i == 0) return false;
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
     int num = 4;
     System.out.print("Is " + num + " prime? ");
     System.out.println(primeNaive(num));
 
-
     int num2 = 7;
     System.out.print("Is " + num2 + " prime? ");
     System.out.println(primeNaive(num2));
+
+    int num3 = 6;
+    System.out.print("Is " + num3 + " prime? ");
+    System.out.println(primeBetter(num3));
+
+    int num4 = 11;
+    System.out.print("Is " + num4 + " prime? ");
+    System.out.println(primeBetter(num4));
   }
 }
