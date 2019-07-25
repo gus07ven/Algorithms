f6984edeb8d2ef773e63d23a455ee55384cf73a8
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-25 13:00:56 -0500

    Add egg drop class

---
+++ src/EggDrop.java
@@ -0,0 +1,36 @@
+public class EggDrop {
+
+  int breakingPoint = 100;
+  int countDrops = 0;
+
+  public boolean drop(int floor){
+    countDrops++;
+    return floor >= breakingPoint;
+  }
+
+  public int findBreakingPoint(int floors){
+    int interval = 14;
+    int previousFloor = 0;
+    int egg1 = interval;
+
+    // Drop egg1 at decreasing intervals
+    while(!drop(egg1) && egg1 <= floors){
+      interval -= 1;
+      previousFloor = egg1;
+      egg1 += interval;
+    }
+
+    int egg2 = previousFloor + 1;
+    while(egg2 < egg1 && egg2 <= floors && !drop(egg2)){
+      egg2 += 1;
+    }
+
+    return egg2 > floors ? -1 : egg2;
+  }
+
+  public static void main(String[] args) {
+    EggDrop ed = new EggDrop();
+    System.out.println(ed.findBreakingPoint(100));
+    System.out.println(ed.countDrops);
+  }
+}
