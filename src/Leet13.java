dba2f0f520e034809b1e5243f6b3288a769ecca9
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-26 14:43:16 -0500

    Roman to Integer more readable solution

--- src/Leet13.java
+++ src/Leet13.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class Leet13 {
 
   public static int romanToInt(String romanNum){
@@ -60,6 +62,44 @@ public class Leet13 {
     }
   }
 
+  public static int betterRomToInt(String romStr){
+    if(romStr.isEmpty()) return 0;
+
+    return calculateTotal(convertToIntMap(romStr));
+  }
+
+  private static HashMap<Integer, Integer> convertToIntMap(String romStr){
+    HashMap<Integer, Integer> values = new HashMap<>();
+    char[] romArr = romStr.toCharArray();
+    for (int i = 0; i < romArr.length; i++) {
+      char c = romArr[i];
+      if(c == 'I') values.put(i,values.getOrDefault(i, 0) + 1);
+      else if(c == 'V') values.put(i,values.getOrDefault(i, 0) + 5);
+      else if(c == 'X') values.put(i,values.getOrDefault(i, 0) + 10);
+      else if(c == 'L') values.put(i,values.getOrDefault(i, 0) + 50);
+      else if(c == 'C') values.put(i,values.getOrDefault(i, 0) + 100);
+      else if(c == 'D') values.put(i,values.getOrDefault(i, 0) + 500);
+      else values.put(i,values.getOrDefault(i, 0) + 1000);
+    }
+    return values;
+  }
+
+  private static int calculateTotal(HashMap<Integer, Integer> values){
+    int total = 0;
+    for(int i = 0; i < values.size(); i++){
+      int current = values.get(i);
+      int next = i + 1 < values.size()? values.get(i + 1) : 0;
+      if(current < next){
+        total += next - current;
+        i++;
+        continue;
+      } else {
+        total += current;
+      }
+    }
+    return total;
+  }
+
   private static int singleToInteger(char num){
     int total = 0;
     if(num == 'I') total += 1;
@@ -73,7 +113,8 @@ public class Leet13 {
   }
 
   public static void main(String[] args) {
-    String input = "MCMXCIV";
+    String input = "III";
     System.out.println(romanToInt(input));
+    System.out.println(betterRomToInt(input));
   }
 }
