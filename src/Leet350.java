084db5dc8dd9e18401a79c12f81080ba74668427
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-26 14:55:58 -0500

    Set up Leet 350

---
+++ src/Leet350.java
@@ -0,0 +1,12 @@
+public class Leet350 {
+
+  public static int[] getIntersection(int[] arrOne, int[] arrTwo){
+    return null;
+  }
+
+  public static void main(String[] args) {
+    int[] nums1 = {1,2,2,1};
+    int[] nums2 = {2,2};
+    System.out.println(getIntersection(nums1, num2));
+  }
+}
