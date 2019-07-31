d447847b9864e8389dce5d600d4f9357853deb61
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-31 13:48:52 -0500

    Add solution for Leet 141

---
+++ src/Leet141.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+public class Leet141 {
+  public static boolean hasCycle(SLinkedList.Node head) {
+    if(head == null) return false;
+
+    HashMap<SLinkedList.Node,Integer> values = new HashMap<>();
+    SLinkedList.Node root = head;
+    int pos = 0;
+    while(root != null){
+      if(values.containsKey(root)){
+        return true;
+      } else {
+        values.put(root, pos);
+      }
+      pos++;
+      root = root.next;
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    SLinkedList.Node head = new SLinkedList.Node(1);
+    SLinkedList.Node two = new SLinkedList.Node(2);
+    SLinkedList.Node three = new SLinkedList.Node(3);
+    head.next = two;
+    two.next = three;
+
+    System.out.println(hasCycle(head));
+  }
+}
