c15d68491af83137c151f411589b2f80375af8b7
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-07-31 13:23:21 -0500

    Add solution for Leet 38

--- src/Leet38.java
+++ src/Leet38.java
@@ -3,23 +3,31 @@ import java.util.ArrayList;
 public class Leet38 {
 
   public static String countAndSay(int n) {
-
-    ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-    ArrayList<Integer> firstLevel = new ArrayList<>();
-    firstLevel.add(1);
-    result.add(firstLevel);
-
-    for(int i = 1; i < n; i++){
-      ArrayList<Integer> prev = result.get(0);
-      ArrayList<Integer> next = new ArrayList<>();
-      int size = prev.size();
-      int curr = prev.get(i);
+    int count = 1;
+    String str = "1";
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i < n ; i++){
+      sb.setLength(0);
+      count = 1;
+      for(int j = 1; j < str.length(); j++){
+        if(str.charAt(j - 1) != str.charAt(j)){
+          sb.append(count);
+          sb.append(str.charAt(j-1));
+          count = 0;
+        }
+        count++;
+      }
+      if(count > 0){
+        sb.append(count);
+        sb.append(str.charAt(str.length()-1));
+      }
+      str = sb.toString();
     }
-    return "";
+    return str;
   }
 
   public static void main(String[] args) {
-    int n = 3;
+    int n = 5;
     System.out.println(countAndSay(n));
   }
 }
