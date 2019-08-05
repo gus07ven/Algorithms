e3384d6aaa293ea23b0a011ed6ba514f1d6f3d63
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-05 18:17:21 -0500

    Add solution to Leet125

---
+++ src/Leet125.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Leet125 {
+
+  public static boolean isPalindrome(String s) {
+    if(s.isEmpty()) return true;
+
+    ArrayList<Character> alphaNumStr = new ArrayList<>();
+    for(int i = 0; i < s.length(); i++){
+      if(Character.isLetterOrDigit(s.charAt(i))){
+        alphaNumStr.add(s.toLowerCase().charAt(i));
+      }
+    }
+
+    int leftPtr = 0;
+    int rightPtr = alphaNumStr.size() - 1;
+    int mid = alphaNumStr.size() / 2;
+    while(leftPtr <= mid && rightPtr >= mid){
+      if(alphaNumStr.get(leftPtr).equals(alphaNumStr.get(rightPtr))){
+        leftPtr++;
+        rightPtr--;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    String input = "rooo";
+    System.out.println(isPalindrome(input));
+  }
+}
