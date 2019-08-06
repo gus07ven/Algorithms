88ebda570776dd545ca58f4868430cc62652c88e
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-06 17:24:25 -0500

    Add constant space solution 287

--- src/Leet287.java
+++ src/Leet287.java
@@ -25,8 +25,29 @@ public class Leet287 {
     return Arrays.stream(nums).sum() - (((nums.length - 1) * (nums.length)) / 2);
   }
 
+  public static int findDuplicateConstSpaceTwo(int[] nums){
+
+    // Find the intersection point of the two runners.
+    int tortoise = nums[0];
+    int hare = nums[0];
+    do {
+      tortoise = nums[tortoise];
+      hare = nums[nums[hare]];
+    } while (tortoise != hare);
+
+    // Find the "entrance" to the cycle.
+    int ptr1 = nums[0];
+    int ptr2 = tortoise;
+    while (ptr1 != ptr2) {
+      ptr1 = nums[ptr1];
+      ptr2 = nums[ptr2];
+    }
+
+    return ptr1;
+  }
+
   public static void main(String[] args) {
-    int[] nums = {3,1,3,4,2};
-    System.out.println(findDuplicateConstSpace(nums));
+    int[] nums = {2,5,9,6,9,3,8,9,7,1};
+    System.out.println(findDuplicateConstSpaceTwo(nums));
   }
 }
