9f2cf4c76ce4a0894edc97259c9ebc44fde3c091
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-07 17:07:06 -0500

    Solution for Leet 69

--- src/Leet69.java
+++ src/Leet69.java
@@ -1,6 +1,6 @@
 public class Leet69 {
 
-  public static int mySqrt(int x) {
+  public static int mySqrtLinear(int x) {
     if(x == 0) return 0;
 
     int num = 0;
@@ -15,7 +15,7 @@ public class Leet69 {
     return num;
   }
 
-  public static int mySqrt2(int x){
+  public static int mySqrtSqrtComplexity(int x){
     if (x == 0) return 0;
     for (int i = 1; i <= x / i; i++)
       if (i <= x / i && (i + 1) > x / (i + 1))
@@ -23,8 +23,22 @@ public class Leet69 {
     return -1;
   }
 
+  // Using a modification of binary search
+  public static int mySqrtLogComplexity(int x){
+    if(x == 0) return 0;
+    int start = 1;
+    int end = x;
+    while(start < end){
+      int mid = start + (end - start) / 2;
+      if(mid <= x / mid && (mid + 1) > x / (mid + 1)) return mid;
+      else if(mid > x / mid) end = mid;
+      else start = mid + 1;
+    }
+    return start;
+  }
+
   public static void main(String[] args) {
     int input = 8;
-    System.out.println(mySqrt2(input));
+    System.out.println(mySqrtLogComplexity(input));
   }
 }
