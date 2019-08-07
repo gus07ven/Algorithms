f058cc504fc997d6ae792deadfe876fd9df0e3d1
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-07 17:08:31 -0500

    Save changes to 198

--- src/Leet198.java
+++ src/Leet198.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.*;
+import java.util.HashMap;
 
 public class Leet198 {
+
+  // TODO: Finish this one.
+  private static int rob(int[] nums) {
+    if(nums.length == 0) return 0;
+
+    HashMap<Integer,Integer> map = new HashMap<>();
+    for(int i = 0; i < nums.length; i++){
+      map.put(i, nums[i]);
+    }
+
+    Set test = map.entrySet();
+
+    return 1;
+  }
+
+  public static void main(String[] args) {
+    int[] houses = {2,1,1,2};
+    System.out.println(rob(houses));
+  }
 }
