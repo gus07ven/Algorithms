947a30d3810f4d60d3afcc8506701e7c36352ad5
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-08 16:33:29 -0500

    Solution to  771

--- src/Leet771.java
+++ src/Leet771.java
@@ -1,12 +1,32 @@
+import java.util.HashMap;
+
 public class Leet771 {
 
   public static int numJewelsInStones(String J, String S) {
-    return 0;
+    if(J.length() == 0 || S.length() == 0) return 0;
+
+    HashMap<Character,Integer> jewels = new HashMap<>();
+    for(Character c : J.toCharArray()){
+      jewels.put(c, 0);
+    }
+
+    char[] stones = S.toCharArray();
+    for(int i = 0; i < stones.length; i++){
+      if(jewels.containsKey(stones[i])){
+        jewels.replace(stones[i], jewels.get(stones[i]) + 1);
+      }
+    }
+
+    int numJewels = 0;
+    for (Integer i : jewels.values()) {
+      numJewels += i;
+    }
+    return numJewels;
   }
 
   public static void main(String[] args) {
-    String J = "aA";
-    String S = "aAAbbbb";
+    String J = "z";
+    String S = "ZZ";
     System.out.println(numJewelsInStones(J, S));
   }
 }
