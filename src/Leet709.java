b1c51b09788f5800bdb885a4b7784a73cd35b3ad
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-08 17:13:30 -0500

    Add built in solution to 709

--- src/Leet709.java
+++ src/Leet709.java
@@ -15,6 +15,10 @@ public class Leet709 {
     return lowercaseString.toString();
   }
 
+  public static String toLowerCaseBuiltIn(String str){
+    return str.toLowerCase();
+  }
+
   public static void main(String[] args) {
     String input = "HelLO";
     System.out.println(toLowerCase(input));
