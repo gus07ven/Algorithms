8ef7eef2dd2cd59281ec144b29f1c88224f1dab6
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-08 16:24:24 -0500

    Set up 771

---
+++ src/Leet771.java
@@ -0,0 +1,12 @@
+public class Leet771 {
+
+  public static int numJewelsInStones(String J, String S) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    String J = "aA";
+    String S = "aAAbbbb";
+    System.out.println(numJewelsInStones(J, S));
+  }
+}
