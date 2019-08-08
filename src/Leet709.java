f2e746ed96431bf846590f6c5ca9d57937e07a8e
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-08 17:08:40 -0500

    Add solution 709

---
+++ src/Leet709.java
@@ -0,0 +1,22 @@
+public class Leet709 {
+
+  public static String toLowerCase(String str) {
+    if(str.isEmpty()) return "";
+
+    StringBuilder lowercaseString = new StringBuilder();
+    for (Character ch : str.toCharArray()) {
+      if(Character.isUpperCase(ch)){
+        int upper = ch + 32;
+        lowercaseString.append((char) upper);
+      } else {
+        lowercaseString.append(ch);
+      }
+    }
+    return lowercaseString.toString();
+  }
+
+  public static void main(String[] args) {
+    String input = "HelLO";
+    System.out.println(toLowerCase(input));
+  }
+}
