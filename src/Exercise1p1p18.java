580b38638e980983c9ba7eb9883c91db4df6b7c2
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-09 15:36:30 -0500

    Add product solution to 1.1.18

--- src/Exercise1p1p18.java
+++ src/Exercise1p1p18.java
@@ -6,9 +6,16 @@ public class Exercise1p1p18 {
     return mystery(a + a, b / 2) + a;
   }
 
+  public static int mysteryProduct(int a, int b){
+    if(b == 0) return 1;
+    if(b % 2 == 0) return mystery(a * a, b / 2);
+    return mystery(a * a, b / 2) * a;
+  }
+
   public static void main(String[] args) {
     int a = 2;
     int b = 25;
     System.out.println(mystery(a, b));
+    System.out.println(mysteryProduct(a, b));
   }
 }
