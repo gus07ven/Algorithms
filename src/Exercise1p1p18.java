f169d545905d553c7dd758dbb1eb151972a77e10
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-09 15:25:03 -0500

    Add solution to 1.1.18

---
+++ src/Exercise1p1p18.java
@@ -0,0 +1,14 @@
+public class Exercise1p1p18 {
+
+  public static int mystery(int a, int b){
+    if(b == 0) return 0;
+    if(b % 2 == 0) return mystery(a + a, b / 2);
+    return mystery(a + a, b / 2) + a;
+  }
+
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 25;
+    System.out.println(mystery(a, b));
+  }
+}
