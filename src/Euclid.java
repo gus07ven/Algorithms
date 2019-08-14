82beac5c75810b6540b7d208a324e95cbd512cbe
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-14 10:48:53 -0500

    Add euclid GCD iterative solution

--- src/Euclid.java
+++ src/Euclid.java
@@ -1,16 +1,24 @@
 public class Euclid {
 
-  public static int recursiveGCD(int p, int q){
+  private static int recursiveGCD(int p, int q){
     if(q == 0) return p;
-    else
-      System.out.println("p: " + p);
-      System.out.println("p % q: " + (p % q));
-      return gcd(q, p % q);
+    else return recursiveGCD(q, p % q);
   }
 
+  private static int iterativeGDC(int p, int q){
+    while (q != 0) {
+      int temp = q;
+      q = p % q;
+      p = temp;
+    }
+    return p;
+  }
+
+
   public static void main(String[] args) {
-    int p = 102;
+    int p = 21;
     int q = 68;
     System.out.println(recursiveGCD(p, q));
+    System.out.println(iterativeGDC(p, q));
   }
 }
