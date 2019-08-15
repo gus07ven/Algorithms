af7e1e057014595c83d30bada88300af00f1d4e9
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-15 16:46:27 -0500

    Add recursive binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -15,16 +15,30 @@ public class BinarySearch {
     return -1;
   }
 
-  public static void main(String[] args) {
-    In input = new In(args[0]);
-    In values = new In(args[1]);
-    int[] whitelist = input.readAllInts();
-    int[] keys = values.readAllInts();
-    Arrays.sort(whitelist);
-
-    for (Integer key : keys) {
-      if(binarySearch(whitelist, key) == -1)
-        StdOut.println(key);
+  private static int binarySearchRecursive(int[] array, int lo, int hi, int key){
+    if(hi >= lo){
+      int mid = lo + (hi - lo) / 2;
+      if(key == array[mid]) return mid;
+      if(key < array[mid]) return binarySearchRecursive(array, lo, mid - 1, key);
+      return binarySearchRecursive(array, mid + 1, hi, key);
     }
+    return -1;
+  }
+
+  public static void main(String[] args) {
+//    In input = new In(args[0]);
+//    In values = new In(args[1]);
+//    int[] whitelist = input.readAllInts();
+//    int[] keys = values.readAllInts();
+//    Arrays.sort(whitelist);
+//
+//    for (Integer key : keys) {
+//      if(binarySearch(whitelist, key) == -1)
+//        StdOut.println(key);
+//    }
+
+    int[] nums = {1, 2, 3, 4, 5, 6, 7};
+    int key = 6;
+    System.out.println(binarySearchRecursive(nums, 0, nums.length, key));
   }
 }
