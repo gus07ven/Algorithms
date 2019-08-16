a9265b29dc217467857587cc1b4eef1a5cedad8d
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-16 09:35:18 -0500

    Change access modifiers

---
+++ src/Helper.java
@@ -0,0 +1,12 @@
+public class Helper {
+
+  public static boolean less(Comparable v, Comparable w){
+    return v.compareTo(w) < 0;
+  }
+
+  public static void exch(Comparable[] a, int i, int j){
+    Comparable swap = a[i];
+    a[i] = a[j];
+    a[j] = swap;
+  }
+}
