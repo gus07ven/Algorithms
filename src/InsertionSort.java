cb63203c654bc3f57f93e2f335c8949c62fb93ee
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-16 15:21:23 -0500

    Add method overload to sort for insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -10,6 +10,14 @@ public class InsertionSort {
     }
   }
 
+  public static void sort(Comparable[] a, int lo, int hi) {
+    for (int i = lo + 1; i < hi; i++) {
+      for (int j = i; j > lo && Helper.less(a[j], a[j-1]); j--) {
+        Helper.exch(a, j, j-1);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     Integer[] nums = {7, 10, 5, 3, 8, 4, 2, 9, 6};
     InsertionSort.sort(nums);
