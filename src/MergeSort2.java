54aacec21d08989795b5ef3d3e0ac7bc7db41153
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-16 15:22:49 -0500

    Add optimization to merge sort2

--- src/MergeSort2.java
+++ src/MergeSort2.java
@@ -1,12 +1,17 @@
 public class MergeSort2 {
 
+  private static final int CUTOFF = 7;
+
   public static void sort(Comparable[] a){
     Comparable[] aux = new Comparable[a.length];
     sort(a, aux, 0, a.length - 1);
   }
 
   private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi){
-    if(hi <= lo) return;
+    if(hi <= lo + CUTOFF - 1){
+      InsertionSort.sort(a, lo, hi);
+      return;
+    }
     int mid = lo + (hi - lo) / 2;
     sort(a, aux, lo, mid);
     sort(a, aux, mid + 1, hi);
