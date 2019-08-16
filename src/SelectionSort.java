b4291d62880075be22784e2b7ce871ec9747aaea
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-16 09:35:55 -0500

    Add selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class SelectionSort {
+
+  public static void sort(Comparable[] a){
+    int N = a.length;
+    for(int i = 0; i < N; i++){
+      int min = i;
+      for(int j = i + 1; j < N; j++){
+        if(Helper.less(a[j], a[min])){
+          min = j;
+        }
+      }
+      Helper.exch(a, i, min);
+    }
+  }
+
+  public static void main(String[] args) {
+    Character[] text = {'S','O','R','T','E','X','A','M','P','L','E'};
+    SelectionSort.sort(text);
+    for (Object i : text) {
+      System.out.println(i);
+    }
+  }
 }
