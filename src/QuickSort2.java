185bb79874f311c9c8cfb8af006607a7cf64ff55
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-19 12:47:15 -0500

    Add quicksort implementation

--- src/QuickSort2.java
+++ src/QuickSort2.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class QuickSort2 {
+
+  public static void sort(Comparable[] a){
+    StdRandom.shuffle(a);
+    sort(a, 0, a.length - 1);
+  }
+
+  private static void sort(Comparable[] a, int lo, int hi){
+    if(hi <= lo) return;
+    int j = partition(a, lo, hi);
+    sort(a, lo, j - 1);
+    sort(a, j + 1, hi);
+  }
 }
