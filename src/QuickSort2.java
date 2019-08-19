206feb1c0c70fe6dfa56c522ddf82deb2dcd5bee
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-19 13:24:12 -0500

    Add partition to quicksort

--- src/QuickSort2.java
+++ src/QuickSort2.java
@@ -11,4 +11,21 @@ public class QuickSort2 {
     sort(a, lo, j - 1);
     sort(a, j + 1, hi);
   }
+
+  private static int partition(Comparable[] a, int lo, int hi){
+    int i = lo;
+    int j = hi + 1;
+    while(true){
+      while(Helper.less(a[++i], a[lo])){
+        if(i == hi) break;
+      }
+      while(Helper.less(a[lo], a[--j])){
+        if(j == lo) break;
+      }
+      if(i >= j) break;     // Check if pointers cross
+      Helper.exch(a, i, j);
+    }
+    Helper.exch(a, lo, j);  // Swap with partitioning item
+    return j;
+  }
 }
