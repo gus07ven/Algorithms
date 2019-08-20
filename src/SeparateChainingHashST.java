c7daa702d5fa4e77f279e36d67acf94dc13c57f5
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-20 17:29:30 -0500

    Add hashtable get method

--- src/SeparateChainingHashST.java
+++ src/SeparateChainingHashST.java
@@ -1,7 +1,7 @@
 public class SeparateChainingHashST<Key, Value> {
 
-  private int M = 97;       // number of chains
-  private Node[] st = new Node[M];   // array of chains
+  private int M = 97;                 // number of chains
+  private Node[] st = new Node[M];    // array of chains
 
   private static class Node {
 
@@ -12,6 +12,14 @@ public class SeparateChainingHashST<Key, Value> {
 
   private int hash(Key key){
     return (key.hashCode() & 0x7fffffff) % M;
+  }
+
+  public Value get(Key key){
+    int i = hash(key);
+    for(Node x = st[i]; x != null; x = x.next){
+      if(key.equals(x.key)) return (Value) x.val;
+    }
+    return null;
   }
 
 }
