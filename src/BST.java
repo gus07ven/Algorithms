6df996d9eb07a0ea1e392704d47df2e45858bc67
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-20 14:35:35 -0500

    Add ordered operations

--- src/BST.java
+++ src/BST.java
@@ -103,8 +103,8 @@ public class BST<K extends Comparable<K>, V> {
   private PNode delete(PNode x, K key){
     if(x == null) return null;
     int cmp = key.compareTo(x.key);
-    if(cmp < 0) x.left = delete(x.left, key);
-    else if(cmp > 0) x.right = delete(x.right, key);
+    if      (cmp < 0) x.left = delete(x.left, key);
+    else if (cmp > 0) x.right = delete(x.right, key);
     else {
       if(x.right == null) return x.left;
       if(x.left == null) return x.right;
