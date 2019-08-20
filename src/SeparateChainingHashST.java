b30873cbeeb4d87d52469add71a9cd017739cc4b
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-20 17:33:36 -0500

    Add hashtable put method

--- src/SeparateChainingHashST.java
+++ src/SeparateChainingHashST.java
@@ -8,6 +8,12 @@ public class SeparateChainingHashST<Key, Value> {
     private Object key;
     private Object val;
     private Node next;
+
+    public Node(Object key, Object val, Node next){
+      this.key = key;
+      this.val = val;
+      this.next = next;
+    }
   }
 
   private int hash(Key key){
@@ -22,4 +28,14 @@ public class SeparateChainingHashST<Key, Value> {
     return null;
   }
 
+  public void put(Key key, Value val){
+    int i = hash(key);
+    for(Node x = st[i]; x != null; x = x.next){
+      if(key.equals(x.key)) {
+        x.val = val;
+        return;
+      }
+    }
+    st[i] = new Node(key, val, st[i]);
+  }
 }
