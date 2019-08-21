1dda11352d13e5b487adaf2032e0492790082021
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-21 17:04:24 -0500

    Add solution for 852

--- src/Leet852.java
+++ src/Leet852.java
@@ -1,10 +1,16 @@
 public class Leet852 {
 
-  public static int peakIndexInMountainArray(int[] A) {
+  private static int peakIndexInMountainArray(int[] A) {
     return findPeak(A, 0, A.length - 1);
   }
 
-  public static int findPeak(int[] A, int lo, int hi){
+  private static int findPeak(int[] A, int lo, int hi){
+    if(hi >= lo){
+      int mid = lo + (hi - lo)/2;
+      if((A[mid - 1] < A[mid]) && (A[mid] > A[mid + 1])) return mid;
+      else if((A[mid - 1] > A[mid]) && (A[mid] > A[mid + 1])) return findPeak(A, lo, mid - 1);
+      else if((A[mid - 1] < A[mid]) && (A[mid] < A[mid + 1])) return findPeak(A, mid + 1, hi);
+    }
     return -1;
   }
 
