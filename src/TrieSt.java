9cedd4ce4921b1af2efec0c99ee1c46773d05ff2
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-27 16:27:52 -0500

    Add methods to TrieST class

--- src/TrieSt.java
+++ src/TrieSt.java
@@ -22,4 +22,21 @@ public class TrieSt<Value> {
     x.next[c] = put(x.next[c], key, val, d + 1);
     return x;
   }
+
+  public boolean contains(String key){
+    return get(key) != null;
+  }
+
+  public Value get(String key){
+    Node x = get(root, key, 0);
+    if(x == null) return null;
+    return (Value) x.val;
+  }
+
+  private Node get(Node x, String key, int d){
+    if(x == null) return null;
+    if(d == key.length()) return x;
+    char c = key.charAt(d);
+    return get(x.next[c], key, d + 1);
+  }
 }
