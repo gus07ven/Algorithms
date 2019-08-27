f6d3001c1affc841be519deda576496653d99219
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-27 11:20:03 -0500

    Add longest common prefix class

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+public class LongestCommonPrefix {
+
+  public static int lcprefix(String first, String second){
+    return 0;
+  }
+
+
+  public static void main(String[] args) {
+    String first = "prefetch";
+    String second = "prefix";
+    System.out.println();
+  }
+}
