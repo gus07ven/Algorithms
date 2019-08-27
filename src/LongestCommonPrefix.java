3b0c95bf58b64b3cbb9059d5f4350272e4f4744e
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-27 11:22:16 -0500

    Add solution to lcp class

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,13 +1,17 @@
 public class LongestCommonPrefix {
 
   public static int lcprefix(String first, String second){
-    return 0;
+    int N = Math.min(first.length(), second.length());
+    for(int i = 0; i < N; i++){
+      if(first.charAt(i) != second.charAt(i)) return i;
+    }
+    return N;
   }
 
 
   public static void main(String[] args) {
     String first = "prefetch";
     String second = "prefix";
-    System.out.println();
+    System.out.println(lcprefix(first, second));
   }
 }
