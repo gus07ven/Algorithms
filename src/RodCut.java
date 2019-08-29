ec4bf2cbd20f304333c201a6e9b5095492d4a1dc
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-29 13:13:07 -0500

    Add top down method solution

--- src/RodCut.java
+++ src/RodCut.java
@@ -10,6 +10,15 @@ public class RodCut {
     this.rodLength = rodLength;
   }
 
+  private int topDownDynProgImp(HashMap<Integer,Integer> priceTable, int rodLength){
+    if(rodLength == 0) return 0;
+
+    int maxRevenue = 0;
+    for(int i = 1; i < rodLength; i++){
+      maxRevenue = Math.max(maxRevenue, priceTable.get(i) + topDownDynProgImp(priceTable, rodLength - i));
+    }
+    return maxRevenue;
+  }
 
   public static void main(String[] args) {
     HashMap<Integer,Integer> priceTable = new HashMap<>();
@@ -25,5 +34,6 @@ public class RodCut {
     priceTable.put(10, 30);
     int rodLength = 7;
     RodCut rc = new RodCut(priceTable, rodLength);
+    System.out.println(rc.topDownDynProgImp(rc.priceTable, rc.rodLength));
   }
 }
