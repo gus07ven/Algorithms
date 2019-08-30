406e318646bd5e02db0e5eb9b8b5db6d35d19e0e
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-30 13:15:54 -0500

    Add Leet746 solution optimal space

--- src/Leet746.java
+++ src/Leet746.java
@@ -12,10 +12,27 @@ public class Leet746 {
     return mc[cost.length];
   }
 
+  public static int minCostClimbingStairsOptSpace(int[] cost) {
+    int a = cost[0];
+    int b = cost[1];
+    int result = Math.min(a, b);
+
+    for (int i = 2; i <= cost.length; i++) {
+      int c = i == cost.length? 0: cost[i];
+      result = Math.min(a + c, b + c);
+      a = b;
+      b = result;
+    }
+
+    return result;
+  }
+
   public static void main(String[] args) {
     int[] cost = {10, 15, 20};
     int[] cost2 = {1, 100, 1, 1, 1, 100, 1, 1, 100, 1};
     System.out.println(minCostClimbingStairs(cost));
     System.out.println(minCostClimbingStairs(cost2));
+    System.out.println(minCostClimbingStairsOptSpace(cost));
+    System.out.println(minCostClimbingStairsOptSpace(cost2));
   }
 }
