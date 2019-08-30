e7326489eeea4157f53168b0f50c22304168f709
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-08-30 15:23:04 -0500

    Add optimal solution to Leet 392

--- src/Leet392.java
+++ src/Leet392.java
@@ -1,6 +1,7 @@
 public class Leet392 {
 
-  public static boolean isSubsequence(String s, String t) {
+  // 12 ms to solve problem
+  private static boolean isSubsequence(String s, String t) {
     if(s.length() == 0) return true;
     if(t.length() == 0) return false;
 
@@ -14,9 +15,25 @@ public class Leet392 {
     return false;
   }
 
+  // 0 ms to solve problem
+  private static boolean isSubsequenceOptimized(String s, String t) {
+    if(s.length() == 0) return true;
+    if(t.length() == 0) return false;
+
+    int tIndex = -1;
+    for (char c : s.toCharArray()) {
+      tIndex = t.indexOf(c, tIndex + 1);
+      if (tIndex < 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) {
     String s = "ace";
     String t = "abcde";
     System.out.println(isSubsequence(s,t));
+    System.out.println(isSubsequenceOptimized(s,t));
   }
 }
