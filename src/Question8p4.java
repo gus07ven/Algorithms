064403a42e8ead3e671dabf0a255eb1c0c7835ce
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-02 16:54:13 -0500

    Add changes to 8.4

--- src/Question8p4.java
+++ src/Question8p4.java
@@ -1,3 +1,5 @@
+import sun.rmi.server.InactiveGroupException;
+
 import java.util.ArrayList;
 
 public class Question8p4 {
@@ -22,11 +24,35 @@ public class Question8p4 {
     return allsubsets;
   }
 
+  public static ArrayList<ArrayList<Integer>> getSubsets2(ArrayList<Integer> set){
+    ArrayList<ArrayList<Integer>> allsubsets = new ArrayList<ArrayList<Integer>>();
+    int max = 1 << set.size();    // Compute 2^n
+    for(int k = 0; k < max; k++){
+      ArrayList<Integer> subset = convertIntToSet(k, set);
+      allsubsets.add(subset);
+    }
+    return allsubsets;
+  }
+
+  public static ArrayList<Integer> convertIntToSet(int x, ArrayList<Integer> set){
+    ArrayList<Integer> subset = new ArrayList<Integer>();
+    int index = 0;
+    for(int k = x; k > 0; k >>= 1){
+      if((k & 1) == 1){
+        subset.add(set.get(index));
+      }
+      index++;
+    }
+    return subset;
+  }
+
 
   public static void main(String[] args) {
     ArrayList<Integer> set = new ArrayList<>();
     set.add(1);
     set.add(2);
     System.out.println(getSubsets(set, 0));
+
+    System.out.println(getSubsets2(set));
   }
 }
