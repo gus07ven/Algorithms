7bbf0b3ef88990f9d05a3e16c53cd18a9a992909
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-03 11:04:21 -0500

    Add Leet 198 top down DP solutions

--- src/Leet198.java
+++ src/Leet198.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
 
 public class Leet198 {
+
+  private static int[] memo;
 
   private static int rob(int[] nums) {
     return rob(nums, nums.length - 1);
@@ -12,8 +15,27 @@ public class Leet198 {
     return Math.max(rob(nums, i - 2) + nums[i], rob(nums, i - 1));
   }
 
+  private static int robMemo(int[] nums){
+    memo = new int[nums.length + 1];
+    Arrays.fill(memo, -1);
+    return rob(nums, nums.length - 1);
+  }
+
+  private static int robMemo(int[] nums, int i){
+    if(i < 0){
+      return 0;
+    }
+    if(memo[i] >= 0){
+      return memo[i];
+    }
+    int result = Math.max(robMemo(nums, i - 2) + nums[i], robMemo(nums, i - 1));
+    memo[i] = result;
+    return result;
+  }
+
   public static void main(String[] args) {
     int[] houses = {2,7,9,3,1};
-    System.out.println(rob(houses));
+    System.out.println("Top down DP solution: " + rob(houses));
+    System.out.println("Top down DP memoized solution: " + robMemo(houses));
   }
 }
