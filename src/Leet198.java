f519ef7d057e6130a918c9e5418e83f91aa0487a
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-03 11:50:39 -0500

    Add Leet 198 bottom up DP iterative solutions

--- src/Leet198.java
+++ src/Leet198.java
@@ -18,7 +18,7 @@ public class Leet198 {
   private static int robMemo(int[] nums){
     memo = new int[nums.length + 1];
     Arrays.fill(memo, -1);
-    return rob(nums, nums.length - 1);
+    return robMemo(nums, nums.length - 1);
   }
 
   private static int robMemo(int[] nums, int i){
@@ -33,9 +33,22 @@ public class Leet198 {
     return result;
   }
 
+  private static int robIter(int[] nums){
+    if(nums.length == 0) return 0;
+    int[] memo = new int[nums.length + 1];
+    memo[0] = 0;
+    memo[1] = nums[0];
+    for(int i = 1; i < nums.length; i++){
+      int val = nums[i];
+      memo[i + 1] = Math.max(memo[i], memo[i - 1] + val);
+    }
+    return memo[nums.length];
+  }
+
   public static void main(String[] args) {
     int[] houses = {2,7,9,3,1};
     System.out.println("Top down DP solution: " + rob(houses));
     System.out.println("Top down DP memoized solution: " + robMemo(houses));
+    System.out.println("Bottom up DP iterative memoized solution: " + robIter(houses));
   }
 }
