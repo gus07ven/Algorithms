e55add3f92df2d5ccc5b4ef38c2bb98b29432006
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-04 16:43:27 -0500

    Add sumRangeTwo method to 303

--- src/Leet303.java
+++ src/Leet303.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class Leet303 {
 
   private Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+  private int[] sum;
 
   public Leet303(int[] nums){
     for(int i = 0; i < nums.length; i++){
@@ -16,8 +17,19 @@ public class Leet303 {
     }
   }
 
+  public Leet303(int[] nums, int filler){
+    sum = new int[nums.length + 1];
+    for(int i = 0; i < nums.length; i++){
+      sum[i + 1] = sum[i] + nums[i];
+    }
+  }
+
   public int sumRange(int i, int j){
     return map.get(new Pair<>(i,j));
+  }
+
+  public int sumRangeTwo(int i, int j){
+    return sum[j + 1] - sum[i];
   }
 
   public static void main(String[] args) {
@@ -26,5 +38,13 @@ public class Leet303 {
     System.out.println(le.sumRange(0, 2));
     System.out.println(le.sumRange(2, 5));
     System.out.println(le.sumRange(0, 5));
+
+    // Optimizing space
+    System.out.println();
+    System.out.println("Optimizing space complexity: ");
+    Leet303 le2 = new Leet303(nums, 0);
+    System.out.println(le2.sumRangeTwo(0, 2));
+    System.out.println(le2.sumRangeTwo(2, 5));
+    System.out.println(le2.sumRangeTwo(0, 5));
   }
 }
