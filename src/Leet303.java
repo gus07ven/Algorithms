4d1edb370b25364c63de6600ac4af73486bb1a7c
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-04 16:23:30 -0500

    Add constructor for 303

--- src/Leet303.java
+++ src/Leet303.java
@@ -1,5 +1,20 @@
+import javafx.util.Pair;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Leet303 {
+
+  private Map<Pair<Integer, Integer>, Integer> map = new HashMap<>();
+
+  public Leet303(int[] nums){
+    for(int i = 0; i < nums.length; i++){
+      int totalSoFar = 0;
+      for(int j = i; j < nums.length; j++){
+        totalSoFar += nums[j];
+        map.put(new Pair<>(i, j), totalSoFar);
+      }
+    }
+  }
 
   public int sumRange(int i, int j){
     return 0;
@@ -7,5 +22,6 @@ public class Leet303 {
 
   public static void main(String[] args) {
     int[] nums = {-2, 0, 3, -5, 2, -1};
+    Leet303 le = new Leet303(nums);
   }
 }
