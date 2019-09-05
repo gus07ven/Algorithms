0cf254cdd93fe11907d3b9b3d8c56a7772ad2764
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-05 13:51:38 -0500

    Add initial set up for Leet 46

--- src/Leet46.java
+++ src/Leet46.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Leet46 {
+
+  public static List<List<Integer>> permute(int[] nums){
+    return new ArrayList<>();
+  }
+
+    public static void main(String[] args) {
+      int[] input = {1,2,3};
+
+      List<List<Integer>> result = permute(input);
+
+      for (List<Integer> a : result) {
+        System.out.println();
+        for(Integer i : a){
+          System.out.print(i + " ");
+        }
+      }
+    }
 }
