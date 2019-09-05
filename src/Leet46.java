d3f6129fe22cec34c246e587e9825ab5b0d09965
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-05 16:05:46 -0500

    Add initial solution for Leet 46

--- src/Leet46.java
+++ src/Leet46.java
@@ -1,13 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Leet46 {
 
   public static List<List<Integer>> permute(int[] nums){
-    return new ArrayList<>();
+    List<List<Integer>> results = new ArrayList<>();
+    backtrack(results, new ArrayList<Integer>(), new HashSet<>(), nums);
+    return results;
   }
 
-    public static void main(String[] args) {
+  private static void backtrack(List<List<Integer>> results, List<Integer> soFar, Set<Integer> soFarSet, int [] nums){
+    if(soFar.size() == nums.length){
+      results.add(new ArrayList<>(soFar));
+    } else{
+      for(int i = 0; i < nums.length; i++){
+        if(soFarSet.contains(nums[i])) continue;
+        soFarSet.add(nums[i]);
+        soFar.add(nums[i]);
+        backtrack(results, soFar, soFarSet, nums);
+        soFarSet.remove(soFar.get(soFar.size() - 1));
+        soFar.remove(soFar.size() - 1);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
       int[] input = {1,2,3};
 
       List<List<Integer>> result = permute(input);
