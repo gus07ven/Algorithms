ec4692679114a8eba08e1936aa19a071394eb540
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-06 17:18:57 -0500

    Add skeleton for ColoredMap

--- src/ColoredMap.java
+++ src/ColoredMap.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class ColoredMap {
+
+  private int[][] map;
+
+  public void createMap(){
+    map = new int[7][];
+    map[0] = new int[] { 1, 4, 2, 5};
+    map[1] = new int[] { 0, 4, 6, 5};
+    map[2] = new int[] { 0, 4, 3, 6, 5};
+    map[3] = new int[] {2, 4, 6};
+    map[4] = new int[] {0, 1, 6, 3, 2};
+    map[5] = new int[] {2, 6, 1, 0};
+    map[6] = new int[] {2, 3, 4, 1, 5};
+  }
+
 }
