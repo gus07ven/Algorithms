683c6a64a3366464c3141ddf42488068cfc4f9f3
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-06 15:20:03 -0500

    Add solution for StrPerm

--- src/StrPerm.java
+++ src/StrPerm.java
@@ -1,7 +1,14 @@
 public class StrPerm {
 
   public static void recPermute(String soFar, String rest){
-
+    if(rest.length() == 0){
+      System.out.println(soFar);
+    } else {
+      for (int i = 0; i < rest.length(); i++) {
+        String remaining = rest.substring(0, i) + rest.substring(i + 1);
+        recPermute(soFar + rest.substring(i, i + 1), remaining);
+      }
+    }
   }
 
   public static void main(String[] args) {
