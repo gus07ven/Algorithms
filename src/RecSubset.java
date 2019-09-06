bc55eb0c9df7e9b62ca6bfad620ef68036978b69
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-06 15:56:36 -0500

    Add RecSubset method

--- src/RecSubset.java
+++ src/RecSubset.java
@@ -1,7 +1,13 @@
 public class RecSubset {
 
   public static void recSubsets(String soFar, String rest){
-
+    if(rest.isEmpty()) {
+      System.out.println(soFar);
+    }
+    else {
+      recSubsets(soFar + rest.substring(0, 1), rest.substring(1));
+      recSubsets(soFar, rest.substring(1));
+    }
   }
 
   public static void main(String[] args) {
