21a9c9777da8b96b505bd357e2a06a36d382c9dd
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-06 15:19:26 -0500

    Add set up for StrPerm

---
+++ src/StrPerm.java
@@ -0,0 +1,11 @@
+public class StrPerm {
+
+  public static void recPermute(String soFar, String rest){
+
+  }
+
+  public static void main(String[] args) {
+    String str = "abc";
+    recPermute("", str);
+  }
+}
