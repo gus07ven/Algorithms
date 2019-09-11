1712afb992e4f2cc536f4d55ec0265d8eed854e9
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-11 13:01:19 -0500

    Add changes to Leet 47

--- src/Leet47.java
+++ src/Leet47.java
@@ -1,16 +1,33 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Leet47 {
 
-  public List<List<Integer>> permuteUnique(int[] nums) {
-    List<List<Integer>> results = new ArrayList<>();
-    //    backtrack(results, new ArrayList<>(), nums);
-    return results;
+  public static List<List<Integer>> permuteUnique(int[] nums) {
+    List<List<Integer>> list = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+    return list;
+  }
+
+  private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+    if(tempList.size() == nums.length){
+      list.add(new ArrayList<>(tempList));
+    } else{
+      for(int i = 0; i < nums.length; i++){
+        if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+        used[i] = true;
+        tempList.add(nums[i]);
+        backtrack(list, tempList, nums, used);
+        used[i] = false;
+        tempList.remove(tempList.size() - 1);
+      }
+    }
   }
 
   public static void main(String[] args) {
-    int[] input = {1, 1, 2};
+    int[] input = {1, 1, 2, 2};
 
     List<List<Integer>> result = permuteUnique(input);
 
