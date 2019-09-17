2f3a8fa6411f4113d8cd0580c2d04fdf74673b1b
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-09-17 17:44:30 -0500

    Add getPopulation method to Country

--- src/Country.java
+++ src/Country.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Country {
 
   private int population;
@@ -14,5 +18,23 @@ public class Country {
 
   public String getContinent(){
     return this.continent;
+  }
+
+  public static int getPopulation(List<Country> countries, String continent){
+    return countries.stream().map(c -> c.getContinent().equals(continent)? c.getPopulation() : 0)
+            .reduce(0, (a, b) -> a + b);
+  }
+
+  public static void main(String[] args) {
+    Country usa = new Country(300, "North America");
+    Country canada = new Country(100, "North America");
+    Country venezuela = new Country(30, "South America");
+    List<Country> countries = new ArrayList<>();
+    countries.add(usa);
+    countries.add(canada);
+    countries.add(venezuela);
+
+    System.out.println(Country.getPopulation(countries, "South America"));
+    System.out.println(Country.getPopulation(countries, "North America"));
   }
 }
